44f4060c8152a6566146bcc4669300e04b74f1ab
Walfarid Hermawan Limbong <140688161+Walfarid@users.noreply.github.com>
2026-02-12 10:02:18 +0800

    Revert "Feature/old register"

--- lib/core/usecase/usecase.dart
+++
@@ -1,9 +0,0 @@
-import 'package:chronoflow/core/errors/failures.dart';
-import 'package:fpdart/fpdart.dart';
-
-// ignore: one_member_abstracts
-abstract class UseCase<T, Params> {
-  Future<Either<Failure, T>> call(Params params);
-}
-
-class NoParams {}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,20 +5,26 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:chronoflow/core/di/service_locator.dart';
-import 'package:chronoflow/features/app/app.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-void main() {
-  testWidgets('App starts on login page', (tester) async {
-    // Build our app and trigger a frame.
-    WidgetsFlutterBinding.ensureInitialized();
-    await setupServiceLocator();
-    await tester.pumpWidget(const MainApp());
-    await tester.pumpAndSettle();
+import 'package:chronoflow/main.dart';
 
-    // Verify that the app initially routes to the login page
-    expect(find.text('Login'), findsWidgets);
+void main() {
+  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(const MyApp());
+
+    // Verify that our counter starts at 0.
+    expect(find.text('0'), findsOneWidget);
+    expect(find.text('1'), findsNothing);
+
+    // Tap the '+' icon and trigger a frame.
+    await tester.tap(find.byIcon(Icons.add));
+    await tester.pump();
+
+    // Verify that our counter has incremented.
+    expect(find.text('0'), findsNothing);
+    expect(find.text('1'), findsOneWidget);
   });
 }
